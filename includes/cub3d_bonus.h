01eed9a407783f1591031fe3263ace204bbb7e4c
lo <vladim1r.gribkov@yandex.ru>
2023-10-02 16:54:37 +0400

    should work def mlx

--- includes/cub3d_bonus.h
+++ includes/cub3d_bonus.h
@@ -1,12 +1,12 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include <mlx.h>
+# include "../mlx/mlx.h"
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
 # include <fcntl.h>
-# include "../src/gnl/get_next_line.h"
+# include "../src/gnl/get_next_line_bonus.h"
 # include <math.h>
 
 # define H  800
